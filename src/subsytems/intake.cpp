1fe6d0135a3745178932b340c3db4f5309fe083f
priyanshudey11 <64995031+priyanshudey11@users.noreply.github.com>
2021-03-12 16:30:24 -0500

    Update intake.cpp
    so it stops completely and does not start again, moved buttons to shoulder

--- src/subsytems/intake.cpp
+++ src/subsytems/intake.cpp
@@ -7,25 +7,30 @@ void setIntake(int power)
     }
 void setIntakeMotors()
   {
-      int intakepower= -12000.0 / 127.0;
-      //stops
-      if(mainController.get_digital(E_CONTROLLER_DIGITAL_A))
-      {
-        intakepower=0;
-      }
+       int intakepower= -9999999.0 / 127.0;
+      //stops with and starts after 5 secounds 
+      if(mainController.get_digital(E_CONTROLLER_DIGITAL_R2))
+        {
+          intakepower=0;
+          delay(5);
+          intakepower= -9999999.0 / 127.0;
+        }
+        //stops 
+      else if(mainController.get_digital(E_CONTROLLER_DIGITAL_A))
+        {
+          intakepower=0;
+        }
       //reverses the motors
-      else if (mainController.get_digital(E_CONTROLLER_DIGITAL_B))
-      {
-        intakepower=12000.0 / 127.0;
-      }
+      else if (mainController.get_digital(E_CONTROLLER_DIGITAL_L1))
+        {
+          intakepower=9999999.0 / 127.0;
+        }
       //restarts the motors
-      else if (mainController.get_digital(E_CONTROLLER_DIGITAL_X))
-      {
-        intakepower= -12000.0 / 127.0;
-      }
-      else
-      {
-        intakepower= -12000.0 / 127.0;
-      }
+      else if (mainController.get_digital(E_CONTROLLER_DIGITAL_R1))
+        {
+          intakepower= -9999999.0 / 127.0;
+        }
       setIntake (intakepower);
+  }
+/*
   }
