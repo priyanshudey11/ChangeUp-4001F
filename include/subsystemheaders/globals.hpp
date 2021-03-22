3cb06ba5e142fc9d5f849fa40348bdfff7810b52
priyanshudey11 <64995031+priyanshudey11@users.noreply.github.com>
2021-03-22 11:04:22 -0400

    Add files via upload

---
+++ include/subsystemheaders/globals.hpp
@@ -0,0 +1,23 @@
+//declares all the motors and other parts in the robot
+#include "main.h"
+using namespace pros;
+//motors
+//varibles are looking at the robot from the back (drive)
+extern Motor driveRF;
+//right back
+extern Motor driveRB;
+//left fron
+extern Motor driveLF;
+//leftback
+extern Motor driveLB;
+//intake right
+extern Motor intaker;
+//intake left
+extern Motor intakel;
+//intake main
+extern Motor intakem;
+//Controller
+extern Controller mainController;
+//vision sensor
+//extern Vision sensor1;
+//Miscellaneous
